136bd209135a20c0635ba56fee0a0a7173659202
Laurens Verspeek <l.verspeek@gmail.com>
2023-07-27 17:28:33 +0200

    add get jobs call

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,29 @@
   <div>
     <h1>Nosana Explorer</h1>
     Solana {{ nosana.solana.config.network }}
+    <h2>Jobs</h2>
+    <div v-if="loading">Loading jobs..</div>
+    <div v-else-if="jobs">
+      <div v-for="job in jobs" :key="job.pubkey">- {{ job.pubkey }}</div>
+      <div v-if="!jobs.length">No jobs</div>
+    </div>
+    <div v-else>Could not load jobs</div>
   </div>
 </template>
 
 <script setup lang="ts">
 const nosana = useSDK();
+const loading = ref(false);
+const jobs: Ref<Array<any>> | Ref<null> = ref(null);
+
+const getJobs = async () => {
+  loading.value = true;
+  try {
+    jobs.value = await nosana.solana.getJobs();
+  } catch (e) {
+    console.error(e);
+  }
+  loading.value = false;
+};
+getJobs();
 </script>
