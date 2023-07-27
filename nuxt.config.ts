7b78bde9622f730c3cf67930db2febb20747368a
Laurens Verspeek <l.verspeek@gmail.com>
2023-07-27 14:02:25 +0200

    add google font + sass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ['~/assets/styles/global.scss'],
+  modules: ['@nuxtjs/google-fonts'],
+  googleFonts: {
+    families: {
+      Lexend: [300, 400, 700],
+    },
+  },
 });
