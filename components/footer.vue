69b7dc14df4cad3b7cab72c64318ee565c2964fd
Jairzijp <jairzijp123@gmail.com>
2023-08-01 13:02:58 +0200

    add basic layout

---
+++ components/footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer class="footer">
+    <hr />
+    <div class="content has-text-centered">
+      <p>
+        Powered by <a href="https://nosana.io"><strong>Nosana</strong></a>
+      </p>
+    </div>
+  </footer>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,40 +1,56 @@
 <template>
-  <div>
-    <h1 class="title is-2">Nosana Explorer</h1>
-    Solana {{ nosana.solana.config.network }}
-    <h2>Jobs</h2>
-    <div v-if="jobs">Total jobs {{ jobs.length }}</div>
-    <div v-if="jobs && jobs.length > perPage">
-      <button :disabled="page < 2" @click="page--">Prev</button> Page
-      {{ page }} of
-      {{ Math.ceil(jobs.length / perPage) }}
-      <button
-        :disabled="page >= Math.ceil(jobs.length / perPage)"
-        @click="page++"
-      >
-        Next
-      </button>
-    </div>
-    <div v-if="loading">Loading jobs..</div>
-    <div v-else-if="filteredJobs">
-      <div v-for="(job, i) in filteredJobs" :key="job">
-        <p>
-          <a
-            :href="`https://explorer.solana.com/address/${job}/anchor-account?cluster=devnet`"
-            target="_blank"
+  <section class="py-6">
+    <div class="container">
+      <h1 class="title is-2">Nosana Explorer</h1>
+      <p>Solana {{ nosana.solana.config.network }}</p>
+      <h2 class="subtitle is-4 mt-4 mb-1">Jobs</h2>
+      <div v-if="jobs">Total jobs {{ jobs.length }}</div>
+      <div v-if="jobs && jobs.length > perPage" class="mb-3">
+        <nav
+          class="pagination is-centered"
+          role="navigation"
+          aria-label="pagination"
+          style="width: 400px"
+        >
+          <button
+            :disabled="page < 2"
+            class="pagination-previous"
+            @click="page--"
           >
-            {{ job }} </a
-          ><br />
-          <span v-if="jobData[i]">
-            {{ jobData[i] }}
-          </span>
-          <span v-else> Loading job data.. </span>
-        </p>
+            Previous
+          </button>
+          Page {{ page }} of
+          {{ Math.ceil(jobs.length / perPage) }}
+          <button
+            :disabled="page >= Math.ceil(jobs.length / perPage)"
+            class="pagination-next"
+            @click="page++"
+          >
+            Next page
+          </button>
+        </nav>
       </div>
-      <div v-if="!filteredJobs.length">No jobs</div>
+      <div v-if="loading">Loading jobs..</div>
+      <div v-else-if="filteredJobs">
+        <div v-for="(job, i) in filteredJobs" :key="job">
+          <p>
+            <a
+              :href="`https://explorer.solana.com/address/${job}/anchor-account?cluster=devnet`"
+              target="_blank"
+            >
+              {{ job }} </a
+            ><br />
+            <span v-if="jobData[i]">
+              {{ jobData[i] }}
+            </span>
+            <span v-else> Loading job data.. </span>
+          </p>
+        </div>
+        <div v-if="!filteredJobs.length">No jobs</div>
+      </div>
+      <div v-else>Could not load jobs</div>
     </div>
-    <div v-else>Could not load jobs</div>
-  </div>
+  </section>
 </template>
 
 <script setup lang="ts">
