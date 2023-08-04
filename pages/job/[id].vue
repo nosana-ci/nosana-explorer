f71fbe0658889bc2b61729e4f41525c596147035
Jairzijp <jairzijp123@gmail.com>
2023-08-04 15:05:53 +0200

    jobresults component

--- pages/job/[id].vue
+++ pages/job/[id].vue
@@ -39,117 +39,17 @@
               v-show="activeTab === 'info'"
               class="p-1 py-4 has-background-white-bis"
             >
-              <VueJsonPretty :data="job.ipfsJob" show-icon show-line-number />
+              <VueJsonPretty :data="ipfsJob" show-icon show-line-number />
             </div>
             <div
               v-show="activeTab === 'result'"
               class="p-1 py-4 has-background-white-bis"
             >
-              <div v-if="!job.ipfsResult || !job.ipfsResult.results">
-                No results
-              </div>
-              <div v-else-if="job.ipfsResult.results[0] === 'nos/secret'">
+              <div v-if="!ipfsResult || !ipfsResult.results">No results</div>
+              <div v-else-if="ipfsResult.results[0] === 'nos/secret'">
                 Results are secret
               </div>
-              <div
-                v-for="jobName in (job.ipfsJob.ops.find(
-                  (j) => j.id === 'checkout',
-                )
-                  ? []
-                  : ['checkout']
-                ).concat(job.ipfsJob.ops.map((j) => j.name || j.id))"
-                v-else
-                :key="jobName"
-                style="counter-reset: line"
-              >
-                <template
-                  v-if="
-                    job.ipfsResult &&
-                    job.ipfsResult.results &&
-                    job.ipfsResult.results[jobName] &&
-                    !jobName.endsWith('-volume')
-                  "
-                >
-                  <div class="row-count has-text-link">
-                    <span>- Executed step '{{ jobName }}'</span>
-                  </div>
-                  <div
-                    v-if="
-                      typeof job.ipfsResult.results[jobName][1] === 'string' &&
-                      (!job.ipfsResult.results[jobName][2] ||
-                        (Array.isArray(job.ipfsResult.results[jobName][2]) &&
-                          job.ipfsResult.results[jobName][2].length === 0))
-                    "
-                    class="has-text-danger row-count"
-                  >
-                    <span>{{ job.ipfsResult.results[jobName][1] }}</span>
-                  </div>
-                  <div
-                    v-for="(step, index) in job.ipfsResult.results[
-                      jobName
-                    ][2] && Array.isArray(job.ipfsResult.results[jobName][2])
-                      ? job.ipfsResult.results[jobName][2][1]
-                      : job.ipfsResult.results[jobName][1]"
-                    v-else
-                    :key="index"
-                  >
-                    <span
-                      v-if="
-                        typeof job.ipfsResult.results[jobName][1] ===
-                          'string' &&
-                        job.ipfsResult.results[jobName][2] &&
-                        index === 0
-                      "
-                      class="has-text-danger row-count"
-                    >
-                      {{ job.ipfsResult.results[jobName][1] }}</span
-                    >
-                    <div
-                      v-if="step.cmd && false"
-                      class="row-count"
-                      :class="{
-                        'has-text-accent': !step.status,
-                        'has-text-danger': step.status,
-                      }"
-                    >
-                      <span v-if="step.cmd.cmd" class="has-text-weight-bold">
-                        <span v-if="!step.cmd.cmd.startsWith('sh -c')"
-                          >$ {{ step.cmd.cmd }}</span
-                        >
-                        <i v-else class="has-text-grey"
-                          >executing through sh -c</i
-                        >
-                      </span>
-                      <span v-else class="has-text-weight-bold"
-                        >$ {{ step.cmd }}</span
-                      >
-                    </div>
-                    <div v-if="step.log && Array.isArray(step.log)">
-                      <div
-                        v-for="(log, ik) in step.log"
-                        :key="ik"
-                        class="row-count"
-                        :class="{
-                          'has-text-danger': log[0] === 2 && step.status,
-                        }"
-                      >
-                        <span class="pre" v-html="log[1].slice(0, 10000)" />
-                        <!-- {{ convert.toHtml(log[1].slice(0, 10000)) }} -->
-                      </div>
-                      <div v-if="step.error" class="row-count has-text-danger">
-                        <span class="has-text-weight-bold">{{
-                          step.error
-                        }}</span>
-                      </div>
-                      <div v-if="step.status" class="row-count has-text-danger">
-                        <span class="has-text-weight-bold"
-                          >Exited with code {{ step.status }}</span
-                        >
-                      </div>
-                    </div>
-                  </div>
-                </template>
-              </div>
+              <JobResult v-else :ipfs-result="ipfsResult" :ipfs-job="ipfsJob" />
             </div>
           </div>
         </div>
@@ -168,6 +68,8 @@ import AnsiUp from 'ansi_up';
 const ansi = new AnsiUp();
 const { nosana } = useSDK();
 const job: Ref<Job | null> = ref(null);
+const ipfsJob: Ref<Object | null> = ref(null);
+const ipfsResult: Ref<{ results: any } | null> = ref(null);
 const jobId: Ref<string> = ref('');
 const loading: Ref<boolean> = ref(false);
 const activeTab: Ref<string> = ref('info');
@@ -193,14 +95,14 @@ export default {
     }
 
     try {
-      job.value!.ipfsJob = await nosana.value.ipfs.retrieve(job.value!.ipfsJob);
-      job.value!.ipfsResult = job.value!.ipfsResult
+      ipfsJob.value = await nosana.value.ipfs.retrieve(job.value!.ipfsJob);
+      ipfsResult.value = job.value!.ipfsResult
         ? await nosana.value.ipfs.retrieve(job.value!.ipfsResult)
         : job.value!.ipfsResult;
 
       if (job.value!.ipfsResult) {
-        for (const key in job.value!.ipfsResult.results) {
-          const results = job.value!.ipfsResult.results[key];
+        for (const key in ipfsResult.value!.results) {
+          const results = ipfsResult.value!.results[key];
           if (Array.isArray(results)) {
             if (results[1]) {
               if (Array.isArray(results[1]) || Array.isArray(results[2])) {
@@ -212,9 +114,9 @@ export default {
                     const step = resultsArray[i];
                     if (step.log && Array.isArray(step.log)) {
                       step.log = step.log
-                        .reduce((str, log) => str.concat(log[1]), '')
+                        .reduce((str: any, log: any) => str.concat(log[1]), '')
                         .split('\n')
-                        .map((l) => [1, ansi.ansi_to_html(l)]);
+                        .map((l: any) => [1, ansi.ansi_to_html(l)]);
                     }
                   }
                 }
@@ -227,33 +129,17 @@ export default {
       console.log(error);
     }
     loading.value = false;
-    return { job, jobId, loading, fmtMSS, timestamp, activeTab };
+    return {
+      job,
+      jobId,
+      loading,
+      fmtMSS,
+      timestamp,
+      activeTab,
+      ipfsResult,
+      ipfsJob,
+    };
   },
 };
 </script>
-<style lang="scss" scoped>
-.pre {
-  white-space: pre-wrap;
-}
-.row-count {
-  word-break: break-word;
-  max-width: 85%;
-  padding-left: 40px;
-  &:before {
-    counter-increment: line;
-    font-family: $family-monospace;
-    font-weight: normal;
-    content: counter(line);
-    display: inline-block;
-    padding: 0 0.5em;
-    margin-right: 0.5em;
-    color: $grey !important;
-    min-width: 50px;
-    text-align: right;
-    margin-left: -62px;
-  }
-  &.has-text-danger:before {
-    color: $red !important;
-  }
-}
-</style>
+<style lang="scss" scoped></style>
