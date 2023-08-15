2a3c48bc903f580dae14cdc81e59835a06cfe9ea
Laurens Verspeek <l.verspeek@gmail.com>
2023-08-15 22:25:41 +0200

    remove logs

--- middleware/network.global.ts
+++ middleware/network.global.ts
@@ -1,7 +1,6 @@
 let removedMainnet = false;
 export default defineNuxtRouteMiddleware((to, from) => {
   if (from.query.network === 'devnet' && !to.query.network && !removedMainnet) {
-    console.log('add devnet');
     return navigateTo({
       path: to.path,
       query: { network: from.query.network },
@@ -9,7 +8,6 @@ export default defineNuxtRouteMiddleware((to, from) => {
   }
   removedMainnet = false;
   if (to.query.network === 'mainnet') {
-    console.log('remove mainnet network');
     removedMainnet = true;
     return navigateTo({ path: to.path });
   }
